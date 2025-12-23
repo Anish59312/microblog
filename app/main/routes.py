a20cc36cb48db1506b148550a04df57783c34143
anish <anish31442@gmail.com>
2025-12-23 10:13:55 +0000

    added view for messages

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,13 +1,13 @@
 from flask import jsonify, render_template, flash, redirect, url_for,request, g, current_app
 from app import db
-from app.models import Post, User
+from app.models import Message, Post, User
 from flask_login import current_user, login_required
 from datetime import datetime, timezone
 from flask_babel import get_locale, _
 from langdetect import detect, LangDetectException
 from app.translate import translate_api
 from app.main import bp
-from app.main.forms import EmptyForm, PostForm, EditProfileForm, SearchForm
+from app.main.forms import EmptyForm, MessageForm, PostForm, EditProfileForm, SearchForm
 from app.search import query_index
 
 @bp.before_request
@@ -140,7 +140,6 @@ def translate_text():
 @login_required
 def search():
     if not g.search_form.validate():
-        print("_______________________________", g.search_form.q.data, g.search_form.validate())
         return redirect(url_for('main.explore'))
     page = request.args.get('page', 1, type=int)
     query, total = Post.search(g.search_form.q.data, page,
@@ -153,3 +152,41 @@ def search():
 
     return render_template('search.html', title=_('Search'), posts=posts, next_url=next_url, prev_url=prev_url)  
   
+@bp.route('/user/<username>/popup')
+@login_required 
+def user_popup(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    form = EmptyForm()
+
+    return render_template('user_popup.html', user=user, form=form)
+
+@bp.route('/send_message/<recipient>', methods=['GET', 'POST'])
+@login_required
+def send_message(recipient):
+    user =  User.query.filter_by(username=recipient).first_or_404()
+    form = MessageForm()
+    
+    if form.validate_on_submit():
+        msg = Message(author=current_user, recipient=user, body=form.message.data)
+        db.session.add(msg)
+        db.session.commit()
+        flash(_('Your message has been sent.'))
+        return redirect(url_for('main.user', username=recipient))
+    return render_template('send_message.html', title=_('Send Message'), form=form, recipient=recipient)
+
+@bp.route('/messages')
+@login_required
+def messages():
+    current_user.last_message_read_time = datetime.now(timezone.utc)
+    db.session.commit()
+    page = request.args.get('page', 1, type=int)
+    messages = current_user.messages_received.order_by(
+        Message.timestamp.desc()
+    ).paginate(
+        page=page, per_page=current_app.config['POSTS_PER_PAGE'], error_out=False
+    )
+
+    next_url = url_for('main.messages', page=messages.next_num) if messages.has_next else None
+    prev_url = url_for('main.messages', page=messages.prev_num) if messages.has_prev else None
+
+    return render_template('messages.html', messages=messages.items, next_url=next_url, prev_url=prev_url)
